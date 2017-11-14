c10ecb380cce61d3068f1de9edec6bd17811bc40
Ben Hughes <bwghughes@gmail.com>
2017-11-14 14:02:27 +0200

    Updated batch file

--- tests.bat
+++ tests.bat
@@ -1,5 +1,5 @@
-SET ENVSTATUS_DB_URL=sqlite:///test.db && \
-apistar create_tables && \
-apistar create_envs && \
-py.test --cov=project project tests.py && \
+SET ENVSTATUS_DB_URL=sqlite:///test.db && 
+apistar create_tables && 
+apistar create_envs && 
+py.test --cov=project project tests.py && 
 del test.db
