de25e8eed85eb6c343888e6790c4f446664b3c7f
Ben Hughes <bwghughes@gmail.com>
2017-11-14 14:04:51 +0200

    OMFG

--- tests.bat
+++ tests.bat
@@ -1,5 +1,1 @@
-SET ENVSTATUS_DB_URL=sqlite:///test.db && 
-apistar create_tables && 
-apistar create_envs && 
-py.test --cov=project project tests.py && 
-del test.db
+SET ENVSTATUS_DB_URL=sqlite:///test.db && apistar create_tables && apistar create_envs && py.test --cov=project project tests.py && del test.db
