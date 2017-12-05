b2b602beed5c9fc0a7830336433075f13821ae44
Ben Hughes <accounts@goincremental.com>
2017-12-05 10:20:18 +0200

    Update start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -1,1 +1,1 @@
-SET ENVSTATUS_DB_URL=sqlite:///envstatus.db && apistar create_tables && apistar run
+SET PAGE_NAME="Nedbank E2E" && SET ENVSTATUS_DB_URL=sqlite:///envstatus.db && apistar create_tables && apistar run
